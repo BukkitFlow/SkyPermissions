b4a842786887afe31d072a714a4aa6b39efe7ef7
BuildTools <unconfigured@null.spigotmc.org>
2018-03-04 00:15:22 +0100

    Project Start

---
+++ src/main/java/de/danielflow/project/SkyPermissions/App.java
@@ -0,0 +1,13 @@
+package de.danielflow.project.SkyPermissions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
